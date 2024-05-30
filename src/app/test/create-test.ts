be63c485c93c018f92d875c6a580f54bc39984bc
Sevillano Apolinario Amilcar <amilcar.sevillano@etudiant.univ-rennes1.fr>
2024-05-30 23:20:55 +0200

    update take test

--- src/app/test/create-test.ts
+++ src/app/test/create-test.ts
@@ -9,43 +9,50 @@ import { Store } from '@ngrx/store';
     templateUrl: './create-test.html',
 })
 export class CreateTestComponent {
-    options = ['Orange', 'White', 'Purple'];
-    input1 = 'Orange';
-    options2 = ['Orange', 'White', 'Purple'];
-    input2 = 'Orange';
-    form1!: FormGroup;
+
+    //options = ['Orange', 'White', 'Purple'];
+    //input1 = 'Orange';
+    //options2 = ['Orange', 'White', 'Purple'];
+    //input2 = 'Orange';
+
+    //form1!: FormGroup;
+
+    search = '';
+    basic: FlatpickrOptions;
+
+    formCreateTest!: FormGroup;
+    isSubmitForm = false;
+
     constructor(public storeData: Store<any>, public fb: FormBuilder) {
         this.initStore();
-        this.form1 = this.fb.group({
-            date1: ['2022-07-05'],
-        });
+        /*this.form1 = this.fb.group({
+
+        });*/
         this.initForm();
         this.basic = {
-            defaultDate: '2022-07-05',
+            defaultDate: '2024-06-01',
             dateFormat: 'Y-m-d',
             position: this.store.rtlClass === 'rtl' ? 'auto right' : 'auto left',
         };
     }
-    search = '';
-    basic: FlatpickrOptions;
 
     initForm() {
-        this.form4 = this.fb.group({
-            firstName: ['Shaun', Validators.required],
-            lastName: ['Park', Validators.required],
-            userName: ['', Validators.required],
-            city: ['', Validators.required],
-            state: ['', Validators.required],
-            zip: ['', Validators.required],
-            isTerms: [false, Validators.requiredTrue],
+        this.formCreateTest = this.fb.group({
+            testName: ['', Validators.required],
+            module: ['', Validators.required],
+            //userName: ['', Validators.required],
+            moduleClass: ['', Validators.required],
+            //state: ['', Validators.required],
+            //zip: ['', Validators.required],
+            //isTerms: [false, Validators.requiredTrue],
+            date1: ['2022-07-05',Validators.required],
+            date2: ['2022-07-05',Validators.required],
         });
     }
 
-    form4!: FormGroup;
-    isSubmitForm4 = false;
-    submitForm4() {
-        this.isSubmitForm4 = true;
-        if (this.form4.valid) {
+    submitForm() {
+        this.isSubmitForm = true;
+        if (this.formCreateTest.valid) {
             //form validated success
             this.showMessage('Form submitted successfully.');
         }
@@ -59,41 +66,6 @@ export class CreateTestComponent {
                 this.store = d;
             });
     }
-
-    modules1 = {
-        toolbar: [[{ header: [1, 2, false] }], ['bold', 'italic', 'underline', 'link'], [{ list: 'ordered' }, { list: 'bullet' }], ['clean']],
-    };
-
-    content1 = `<h2>1. MaxSoft is a software company. (20px)</h2>
-            <br />
-            <p>
-              - (X) True
-            </p>
-            <p>
-              - () False
-            </p>
-            <br />
-            <h2>2. The domain of MaxSoft is test automation framework development. (10px)</h2>
-            <p>
-              - (X) True
-            </p>
-            <p>
-              - () False
-            </p>
-            <br />
-            <h2>3. What are the test automation frameworks developped by MaxSoft. (15px)</h2>
-            <p>
-              - [X] IntelliAPI
-            </p>
-            <p>
-              - [] WebBot
-            </p>
-            <p>
-              - [] Gauge
-            </p>
-            <p>
-              - [] Selenium
-            </p>`;
 
 
     createTest(){
