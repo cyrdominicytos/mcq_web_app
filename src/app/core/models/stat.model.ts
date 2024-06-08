dbb38ed28d7011dcb061849d37f855b3cc258b47
Tuo Adama <tuoadama17@gmail.com>
2024-06-09 00:05:22 +0200

    refactor: refactoring

--- src/app/core/models/stat.model.ts
+++ src/app/core/models/stat.model.ts
@@ -1,6 +1,6 @@
 import UnAnswerQuestionModel from './unAnswerQuestion.model';
 import { Qcm } from './qcm.model';
-import QcmStatModel from './QcmStat.model';
+import QcmStatModel from './qcmStat.model';
 import QuestionStat from './questionStat.model';
 
 export default interface Stat {
