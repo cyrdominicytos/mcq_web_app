c567ee4aa35f6342e3eae23bc5caae91e36c571f
Tuo Adama <tuoadama17@gmail.com>
2024-06-10 13:53:39 +0200

    feat: stats view

--- src/app/service/qcm.service.ts
+++ src/app/service/qcm.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import * as url from 'url';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,9 @@ export class QcmService {
   ) { }
 
     public getQcm(id: string|number): any{
-      return this.httpClient.get(url+`/qcm/${id}`)
+      return this.httpClient.get(environment.apiUrl+`/qcm/${id}`)
+    }
+    public getAll(): Observable<any>{
+        return this.httpClient.get(environment.apiUrl+`/qcm`)
     }
 }
