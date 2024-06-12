71a87ee71207a5a71716532cc1c6eb172b876653
Tuo Adama <tuoadama17@gmail.com>
2024-06-12 17:12:37 +0200

    comment service

---
+++ src/app/service/comment.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+    url = "";
+  constructor(
+      private httpClient: HttpClient
+  ) {
+      this.url = environment.apiUrl;
+  }
+
+    validAnswerComment(comment: any){
+        return this.httpClient.put(this.url+"/comments/questions", {
+            ...comment,
+            valid: true,
+        })
+    }
+
+    validQuestionComment(comment: any){
+      return this.httpClient.put(this.url+"/comments/answers", {
+          ...comment,
+          valid: true,
+      })
+    }
+
+    deleteAnswerComment(id: number){
+        return this.httpClient.delete(this.url + `/comments/answers/delete/${id}`)
+    }
+
+    deleteQuestionComment(id: number){
+        return this.httpClient.delete(this.url + `/comments/questions/delete/${id}`)
+    }
+}
